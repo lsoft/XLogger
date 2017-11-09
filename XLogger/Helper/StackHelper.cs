3a6bda1f5d38c31e2775a0305a2014c8f44297d8
lsoft <lsoft@list.ru>
2017-11-09 12:42:52 +0500

    performed improvements in caller's class name retrieve logic

--- XLogger/Helper/StackHelper.cs
+++ XLogger/Helper/StackHelper.cs
@@ -9,9 +9,20 @@ namespace XLogger.Helper
     /// <summary>
     /// Stack-related helper.
     /// </summary>
-    internal static class StackHelper
+    public static class StackHelper
     {
-        public static string GetClassNameFromStack()
+        public static int StackFrame
+        {
+            get;
+            set;
+        }
+
+        static StackHelper()
+        {
+            StackHelper.StackFrame = 2;
+        }
+
+        internal static string GetClassNameFromStack()
         {
             var result = "N/A";
 
@@ -20,7 +31,8 @@ namespace XLogger.Helper
             {
                 var stack = new StackTrace(0, true);
 
-                result = stack.GetFrame(2).GetMethod().DeclaringType.Name;
+                var method = stack.GetFrame(StackHelper.StackFrame).GetMethod();
+                result = GetClassName(method.DeclaringType);
             }
             catch
             {
@@ -31,5 +43,72 @@ namespace XLogger.Helper
             return
                 result;
         }
+
+#if !WindowsCE
+        private static string GetClassName(Type type)
+        {
+            string result;
+
+            try
+            {
+                if (type.IsGenericType)
+                {
+                    var p = string.Join(
+                        ",",
+                        type
+                            //.GetGenericTypeDefinition()
+                            .GetGenericArguments()
+                            .ToList()
+                            .ConvertAll(j => GetClassName(j))
+                            .ToArray());
+
+                    var ppp = FullNameConverter(type.Name);
+                    ppp = ppp.Substring(0, ppp.IndexOf('`'));
+                    ppp = ParameterTypeStringConverter(ppp);
+
+                    result = string.Format(
+                        "{0}<{1}>",
+                        ppp,
+                        p);
+                }
+                else
+                {
+                    result = FullNameConverter(type.Name);
+                }
+            }
+            catch
+            {
+                result = type.Name;
+            }
+
+            return result;
+
+        }
+
+        private static string FullNameConverter(string fullname)
+        {
+            //вложенные классы в рефлексии отображаются через знак плюс
+            var fn0 = fullname.Replace(
+                "+",
+                "."
+                );
+
+            return fn0;
+        }
+
+        private static string ParameterTypeStringConverter(string pt)
+        {
+            var pt0 = pt.Replace(
+                "&",
+                string.Empty);
+
+            var pt1 = pt0.Replace(
+                "+",
+                ".");
+
+            return pt1;
+        }
+#endif
+
     }
 }
